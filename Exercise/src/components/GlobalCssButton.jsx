39154cad5f012282463acbb9ccfbe76934ea58df
Sujit431 <sujitkumarpradhan364@gmail.com>
2026-04-16 12:27:40 +0530

    Delete Exercise directory

--- Exercise/src/components/GlobalCssButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function GlobalCssButton() {
-  return (
-    <div style={{ textAlign: 'center', margin: '15px 0' }}>
-      <button className="btn">Global Blue Button</button>
-    </div>
-  );
-}
