d48998b8083d05e1c80af1810e51a3e2a2173dd7
Sujit-Kumar-Pradhan <sujit431@gmail.com>
2026-04-16 12:19:50 +0530

    Adding Exercise 3

---
+++ Exercise_3/src/components/GlobalCssButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function GlobalCssButton() {
+  return (
+    <div style={{ textAlign: 'center', margin: '15px 0' }}>
+      <button className="btn">Global Blue Button</button>
+    </div>
+  );
+}
